3ef0d8b58de3a78219c8770d3208056729cbd48d
Arda Tugay <ardactugay@gmail.com>
2019-12-12 18:25:35 -0500

    Initial Commit

---
+++ SwiftUICustomTabBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICustomTabBar
+//
+//  Created by Arda Tugay on 12/12/19.
+//  Copyright © 2019 ardatugay. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
